42494990d047374164fbe07f94a55f29a890fed0
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-02-27 23:12:34 +0600

    add imagekit for uplaod image and get link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,29 @@
 const express = require("express")
+const multer = require("multer")
+const uploadFile = require("./services/storage.service")
+
 
 const app = express()
+
+app.use(express.json())
+
+const upload = multer({storage: multer.memoryStorage()})
+
+app.post("/create-post",upload.single('image'), async (req, res) => {
+    // const post = req.body
+    // console.log(post);
+    // console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer)
+    
+    console.log(result);
+    
+
+    res.status(201).json({
+        msg:"post created"
+    })
+    
+})
 
 
 module.exports=app
---
+++ src/services/storage.service.js
@@ -0,0 +1,24 @@
+const  ImageKit = require('@imagekit/nodejs');
+
+const imagekit = new ImageKit({
+    privateKey: `${process.env.IMAGEKIT_PRIVATE_KEY}`, // This is the default and can be omitted
+    
+    
+});
+
+
+
+
+async function uploadFile(buffer) {
+    
+    // console.log(buffer);
+    
+    const res = await imagekit.files.upload({
+        file: buffer.toString("base64"),
+        fileName:"img.jpg"
+    })
+
+    return res
+}
+
+module.exports = uploadFile;
