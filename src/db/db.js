7465e2c6271e0a03515447de56b0650c15c90a39
joynalmdabedin <joynalmdabedin440@gmail.com>
2026-03-01 23:36:22 +0600

    add user model schema

--- src/db/db.js
+++ src/db/db.js
@@ -6,9 +6,16 @@ const DB_URI = process.env.DB_URI
 
 
 async function connectDB() {
-    await mongoose.connect(`${DB_URI}`)
+    try {
+        await mongoose.connect(`${DB_URI}`)
+        console.log("mongodb connected successfully");
+        
+    } catch (error) {
+        console.error("Database connection error",error);
+        
+    }
 
-    console.log("mongodb connected successfully");
+    
     
 }
 
